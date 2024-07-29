8c17c0668f112e7ed5835f691d8855edaf8419ae
Paschalis Tsilias <paschalist0@gmail.com>
2024-07-29 12:56:47 +0300

    Expose the Host of remotecfg's isolated controller with a Getter
    Signed-off-by: Paschalis Tsilias <paschalist0@gmail.com>

--- internal/runtime/alloy_services.go
+++ internal/runtime/alloy_services.go
@@ -70,7 +70,7 @@ func serviceConsumersForGraph(graph *dag.Graph, serviceName string, includePeerS
 // NewController returns a new, unstarted, isolated Alloy controller so that
 // services can instantiate their own components.
 func (f *Runtime) NewController(id string) service.Controller {
-	return serviceController{
+	return ServiceController{
 		f: newController(controllerOptions{
 			Options: Options{
 				ControllerID:    id,
@@ -89,16 +89,18 @@ func (f *Runtime) NewController(id string) service.Controller {
 	}
 }
 
-type serviceController struct {
+type ServiceController struct {
 	f *Runtime
 }
 
-func (sc serviceController) Run(ctx context.Context) { sc.f.Run(ctx) }
-func (sc serviceController) LoadSource(b []byte, args map[string]any) error {
+func (sc ServiceController) Run(ctx context.Context) { sc.f.Run(ctx) }
+func (sc ServiceController) LoadSource(b []byte, args map[string]any) error {
 	source, err := ParseSource("", b)
 	if err != nil {
 		return err
 	}
 	return sc.f.LoadSource(source, args)
 }
-func (sc serviceController) Ready() bool { return sc.f.Ready() }
+func (sc ServiceController) Ready() bool { return sc.f.Ready() }
+
+func (sc ServiceController) GetHost() service.Host { return sc.f }
